323cc583987eae1d44e4073542a4f12dcda6ddb4
ntp_vv <63070054@kmitl.ac.th>
2025-03-29 23:11:24 +0700

    feat: add vercel

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("../config/database");
+const userRoutes = require("../function/user");
+const postRoutes = require("../function/post");
+const reviewRoutes = require("../function/review");
+const locationRouters = require("../function/location");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+connectDB();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.status(200).json(`Server running on port ${PORT}`);
+  });
+
+// Routes
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+app.use("/reviews", reviewRoutes);
+app.use("/locations", locationRouters);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
