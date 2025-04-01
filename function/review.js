907acd2a5f49715fb08b3fbc956378f6fa849f92
ntp_vv <63070054@kmitl.ac.th>
2025-04-02 00:14:44 +0700

    feat: cast object for review

--- function/review.js
+++ function/review.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const Review = require("../model/review");
 const router = express.Router();
+const { ObjectId } = require("mongoose").Types;
 
 router.post("/", async (req, res) => {
   const { reviewerId, userId } = req.body;
 
+  const objectReviewerId = new ObjectId(reviewerId);
+  const objectUserId = new ObjectId(userId);
+
   try {
-    const existingReview = await Review.findOne({ reviewerId, userId });
+    const existingReview = await Review.findOne({ reviewerId: objectReviewerId, userId: objectUserId });
 
     if (existingReview) {
       return res.status(400).json({ error: "This reviewer has already reviewed this user" });
     }
 
-    const newReview = new Review(req.body);
+    const newReview = new Review({
+      ...req.body,
+      reviewerId: objectReviewerId,
+      userId: objectUserId,
+    });
     await newReview.save();
     res.json({ message: "Review added" });
   } catch (error) {
